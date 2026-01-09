b19b844848c31ec5464ace9a59d1347c94a369b3
melekbadreddine <mbadreddine5@gmail.com>
2026-01-09 19:33:00 +0100

    Remove fresh output from the flake

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, fresh, ... }:
+  outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
