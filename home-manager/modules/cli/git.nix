d48f8eeb9c765c698e69f1d210b0d2db121ab94f
melekbadreddine <mbadreddine5@gmail.com>
2026-05-04 18:46:26 +0100

    Refactor git module and remove gh

--- home-manager/modules/cli/git.nix
+++ home-manager/modules/cli/git.nix
@@ -1,5 +1,4 @@
 {osConfig, ...}: let
-  # Fallback to the default NixOS SOPS path if osConfig is not available (e.g., in standalone HM build)
   sopsPath =
     if osConfig != null && osConfig ? sops
     then osConfig.sops.secrets."github/token".path
@@ -7,24 +6,14 @@
 in {
   programs.git = {
     enable = true;
-
     settings = {
       user = {
         name = "melekbadreddine";
         email = "mbadreddine5@gmail.com";
       };
-      init = {
-        defaultBranch = "main";
-      };
-      credential = {
-        helper = ''!f() { [ "$1" = "get" ] && echo "username=melekbadreddine" && echo "password=$(cat ${sopsPath})"; }; f'';
-      };
+      init.defaultBranch = "main";
+      credential.helper = ''!f() { [ "$1" = "get" ] && printf "username=melekbadreddine\npassword=$(cat ${sopsPath})\n"; }; f "$@"'';
     };
-  };
-
-  programs.gh = {
-    enable = true;
-    settings.git_protocol = "ssh";
   };
 
   programs.lazygit.enable = true;
