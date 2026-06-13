691dfe1f158d004cfed52ff74639f17cd01fec13
melekbadreddine <mbadreddine5@gmail.com>
2026-06-13 22:22:51 +0100

    feat(vm): migrate guest services to VMware with open-vm-tools and modesetting driver

--- modules/drivers/vm-guest-services.nix
+++ modules/drivers/vm-guest-services.nix
@@ -9,35 +9,18 @@ in {
   options.vm.guest-services = {
     enable = mkEnableOption "Enable Virtual Machine Guest Services";
   };
-
   config = mkIf cfg.enable {
-    services.qemuGuest.enable = true;
-    services.spice-vdagentd.enable = true;
-
-    # Hardware acceleration for VMs
+    virtualisation.vmware.guest = {
+      enable = true;
+      headless = false;
+    };
+    services.xserver.videoDrivers = ["modesetting"];
     hardware.graphics = {
       enable = true;
-      # 3D acceleration in VMs often requires these for Virgil/SVGA
       enable32Bit = true;
     };
-
-    # VirtualBox Guest Additions
-    virtualisation.virtualbox.guest.dragAndDrop = true;
-
-    # Better VM graphics performance
-    services.xserver.videoDrivers = ["vmware" "modesetting"];
-
     environment.variables = {
-      # Prefer stable software rendering for GPU-sensitive apps under VirtualBox 3D.
-      GSK_RENDERER = "cairo";
-      LIBGL_ALWAYS_SOFTWARE = "1";
-      MESA_LOADER_DRIVER_OVERRIDE = "llvmpipe";
-      WLR_RENDERER_ALLOW_SOFTWARE = "1";
       WLR_NO_HARDWARE_CURSORS = "1";
     };
-
-    # spice-webdavd is disabled due to build failure with davsfs2 (unsupported neon version)
-    # Enable if your system doesn't encounter this issue
-    services.spice-webdavd.enable = false;
   };
 }
